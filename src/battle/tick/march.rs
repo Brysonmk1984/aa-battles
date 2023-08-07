dcdf1047c99836c8da4b5cb7eac43af2688c4375
Bryson Kruk <brysonmk1984@gmail.com>
2023-08-06 18:49:08 -0600

    removed unnecessary code

--- src/battle/tick/march.rs
+++ src/battle/tick/march.rs
@@ -2,11 +2,6 @@ use crate::match_up::match_up::{Battalion, StartingDirection};
 
 pub fn march_phase(army: &mut Vec<Battalion>, starting_direction: &StartingDirection) {
     army.iter_mut().for_each(|a| {
-        let which_army = match starting_direction {
-            StartingDirection::WEST => "1",
-            StartingDirection::EAST => "2",
-        };
-
         if a.is_marching && a.count > 0 {
             a.march(*starting_direction)
         }
